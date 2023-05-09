b6224b2e604cd941512c5e37543993fa8a9cd891
Jalpa Dangariya <jalpa.dangaria@gmail.com>
2023-05-09 13:12:51 +0200

    first commit with Html and CSS

---
+++ script/script.js
@@ -0,0 +1,17 @@
+let displayEl = document.getElementById("display");
+let buttonsEl = document.querySelectorAll("button");
+
+let operatorEl = document.querySelectorAll(".operator");
+let operandEl = document.querySelectorAll(".operand");
+
+
+const buttons= buttonsEl.forEach(
+    button=>{button.addEventListener}
+)
+
+function add(num1, num2){
+    var result = 0;
+    return result= num1 + num2;
+}
+
+console.log(add(operandEl.values,));
